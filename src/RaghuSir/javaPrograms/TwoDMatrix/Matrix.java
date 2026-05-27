f4dafc327826dbed64ec469d05eba48cbfdb6afc
Deepith N <deepithdeekshith@gmail.com>
2026-05-27 09:34:09 +0530

    Added the assignment questions

--- src/RaghuSir/javaPrograms/TwoDMatrix/Matrix.java
+++ src/RaghuSir/javaPrograms/TwoDMatrix/Matrix.java
@@ -68,4 +68,85 @@ public class Matrix {
         }
         return z;
     }
+    static boolean isSquareMat(int[][]x){
+        return x.length == x[0].length;
+    }
+
+    static int[][] subtractMatrix(int[][]x,int[][]y){
+        if(x.length != y.length || x[0].length != y[0].length){
+            return null;
+        }
+        int [][]z = new int[x.length][x[0].length];
+        for(int i=0;i<z.length;i++){
+            for(int j=0;j<z[i].length;j++){
+                z[i][j]=x[i][j]-y[i][j];
+            }
+        }
+        return z;
+    }
+
+    static void evenOddCount(int[][]mat){
+        int evenCount=0;
+        int oddCount=0;
+
+        for(int[] row:mat){
+            for(int elem:row){
+                if(elem%2==0){
+                    evenCount++;
+                }else{
+                    oddCount++;
+                }
+            }
+        }
+        System.out.println("Even count is : "+evenCount);
+        System.out.println("Odd count is : "+oddCount);
+    }
+
+    static  int primeCount(int[][]mat){
+
+        int count = 0;
+
+        for(int[]row :mat){
+            for(int ele:row){
+
+                if(ele >1){
+
+                    boolean isPrime = true;
+                    for(int i=2;i<ele/2;i++){
+                        if(ele%i==0){
+                            isPrime=false;
+                            break;
+                        }
+                    }
+                    if(isPrime){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    static int diseriumCount(int[][]mat){
+
+        int count=0;
+
+        for(int[]row:mat){
+            for(int ele:row){
+
+                String s = String.valueOf(ele);
+                int sum=0;
+
+                for(int i=0;i<s.length();i++){
+                    int digit = s.charAt(i)-'0';
+                    sum+= Math.pow(digit,i+1);
+                }
+                if(sum==ele){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 }
